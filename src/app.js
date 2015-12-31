006458c97d1a3e36c8dfc21d76012af0a9ac29b0
Asmund Berge <powermosfet@users.noreply.github.com>
2015-12-31 10:37:23 +0100

    Basic screen setup
    Cleared the example and added two menu screens, one for listing stops, and one for listing departures for the selected stop.
    
    Just hardcoded content for now.

--- src/app.js
+++ src/app.js
@@ -5,58 +5,42 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
+// var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
-});
-
-main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
+var main = new UI.Menu({
+  backgroundColor: '#658D2E',
+  highlightBackgroundColor: '#C2D100',
+  highlightTextColor: 'black',
     sections: [{
       items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
+        title: 'Lyefjell',
+        subtitle: '234m'
       }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
+        title: 'Heilovegen',
+        subtitle: '423m'
       }]
     }]
   });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+main.show();
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+main.on('select', function(index) {
+  var depList = new UI.Menu({
+  backgroundColor: '#658D2E',
+  highlightBackgroundColor: '#C2D100',
+  highlightTextColor: 'black',
+    sections: [{
+      items: [{
+        title: '50 Bryne',
+        subtitle: '5min'
+      }, {
+        title: '56 Undheim',
+        subtitle: 'ca 13min'
+      },{
+        title: '50 Bryne',
+        subtitle: 'ca 35min'
+      }]
+    }]
+  });
+  depList.show();
 });
