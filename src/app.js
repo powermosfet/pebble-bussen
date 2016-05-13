b72c2b39102c922139c443042790d78bb4165468
Asmund Berge <powermosfet@users.noreply.github.com>
2016-05-13 12:39:39 +0200

    Started hooking it up to the api

--- src/app.js
+++ src/app.js
@@ -5,24 +5,40 @@
  */
 
 var UI = require('ui');
+var ajax = require('ajax');
 // var Vector2 = require('vector2');
+
+var baseUrl = "https://pebble-sanntid-backend.herokuapp.com/";
+var stopsUrl = baseUrl + "api/gtfs/stops/";
+var siriUrl = baseUrl + "api/siri/getstopmonitoring/";
+
+var loadingScreen = new UI.Card({
+  title: "Laster..."
+});
 
 var main = new UI.Menu({
   backgroundColor: '#658D2E',
   highlightBackgroundColor: '#C2D100',
   highlightTextColor: 'black',
-    sections: [{
-      items: [{
-        title: 'Lyefjell',
-        subtitle: '234m'
-      }, {
-        title: 'Heilovegen',
-        subtitle: '423m'
-      }]
-    }]
-  });
+});
 
-main.show();
+loadingScreen.show();
+
+navigator.geolocation.getCurrentPosition(function(loc) {
+  var coords = "" + loc.coords.latitude + "," + loc.coords.longitude;
+  console.log(coords);
+  var url = stopsUrl + coords + "/";
+  console.log("url: " + url);
+  ajax({ url: url, type: 'json' },
+    function(data, status, req) {
+      console.log("Handling response...");
+      main.sections([{
+        items: data.map(function(s){ return { title: s.stop_name, subtitle: "" + s.distance + "m" };})
+      }]);
+      main.show();
+    }
+  );
+});
 
 main.on('select', function(index) {
   var depList = new UI.Menu({
