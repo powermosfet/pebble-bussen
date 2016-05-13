23acdf0ee0470bb4d6113de42a1d6eba7022c07e
Asmund Berge <powermosfet@users.noreply.github.com>
2016-05-13 23:59:51 +0200

    implement realtime info

--- src/app.js
+++ src/app.js
@@ -6,14 +6,15 @@
 
 var UI = require('ui');
 var ajax = require('ajax');
+var moment = require('moment');
+
+var config = require('config.js');
 // var Vector2 = require('vector2');
 
-var baseUrl = "https://pebble-sanntid-backend.herokuapp.com/";
-var stopsUrl = baseUrl + "api/gtfs/stops/";
-var siriUrl = baseUrl + "api/siri/getstopmonitoring/";
-
 var loadingScreen = new UI.Card({
-  title: "Laster..."
+  backgroundColor: '#658D2E',
+  title: "Laster...",
+  subtitle: "henter lokasjon",
 });
 
 var main = new UI.Menu({
@@ -27,36 +28,48 @@ loadingScreen.show();
 navigator.geolocation.getCurrentPosition(function(loc) {
   var coords = "" + loc.coords.latitude + "," + loc.coords.longitude;
   console.log(coords);
-  var url = stopsUrl + coords + "/";
+  var url = config.stopsUrl + "?coords=" + coords;
   console.log("url: " + url);
+  loadingScreen.subtitle("henter busstopp");
+  loadingScreen.backgroundColor('#658D2E');
   ajax({ url: url, type: 'json' },
     function(data, status, req) {
-      console.log("Handling response...");
-      main.sections([{
-        items: data.map(function(s){ return { title: s.stop_name, subtitle: "" + s.distance + "m" };})
-      }]);
+      console.log("Handling gtfs response...");
+      main.json_data = data;
+      main.items(0, data.map(function(s){ return { title: s.stop_name, subtitle: "" + s.distance + "m", stop_id: s.stop_id };}));
       main.show();
+      loadingScreen.hide();
     }
   );
 });
 
-main.on('select', function(index) {
-  var depList = new UI.Menu({
-  backgroundColor: '#658D2E',
-  highlightBackgroundColor: '#C2D100',
-  highlightTextColor: 'black',
-    sections: [{
-      items: [{
-        title: '50 Bryne',
-        subtitle: '5min'
-      }, {
-        title: '56 Undheim',
-        subtitle: 'ca 13min'
-      },{
-        title: '50 Bryne',
-        subtitle: 'ca 35min'
-      }]
-    }]
-  });
-  depList.show();
+main.on('select', function(e) {
+  console.log("Selected");
+  var stop_id = e.item.stop_id;
+  loadingScreen.subtitle('Henter sanntidsdata');
+  loadingScreen.backgroundColor('#658D2E');
+  loadingScreen.show();
+  var url = config.siriUrl + "?stop_id=" + stop_id + "&timestamp=" + (new Date()).toISOString();
+  console.log(url);
+  ajax({ url: url, type: 'json' },
+    function(data, status, req) {
+      console.log("Handling siri response...");
+      var departures = data.map(function(d){
+        return { 
+          title: d.published_line_name + ' ' + d.destination_display, 
+          subtitle: moment.duration(moment() - (d.expected_departure_time || d.aimed_departure_time)).humanize(true) + (d.expected_departure_time? '' : ' (?)'),
+        };
+      });
+      var depList = new UI.Menu({
+        backgroundColor: '#658D2E',
+        highlightBackgroundColor: '#C2D100',
+        highlightTextColor: 'black',
+          sections: [{
+            items: departures
+          }]
+      });
+      depList.show();
+      loadingScreen.hide();
+    }
+  );
 });
---
+++ src/config.js
@@ -0,0 +1,6 @@
+var baseUrl = "https://pebble-sanntid-backend.herokuapp.com/";
+
+module.exports = {
+  stopsUrl: baseUrl + "api/gtfs/stops/closest/",
+  siriUrl: baseUrl + "api/siri/getstopmonitoring/",
+};
